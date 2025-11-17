9ca2db69c9b0b807f322876ffd3017d0f1ccd9d9
Geetansh Chopra <114070333+Orion-Pax01@users.noreply.github.com>
2025-11-17 14:35:14 +0530

    Add basic calculations in basicCalculations.js

---
+++ JS/Basics/basicCalculations.js
@@ -0,0 +1,14 @@
+var x = 2;
+var y = 2;
+
+var z = x + y;
+var a = x - y;
+var b = x * y;
+var c = x / y;
+var d = x % y;
+
+document.writeln("Addition: ", z);
+document.writeln("Subtraction: ", a);
+document.writeln("Multiplication: ", b);
+document.writeln("Division: ", c);
+document.writeln("Modulus: ", d);
