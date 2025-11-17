2179c26b9eeb20a7d8d8904c092f317832fdb853
Geetansh Chopra <114070333+Orion-Pax01@users.noreply.github.com>
2025-11-17 14:34:21 +0530

    Create comparisons.js

---
+++ JS/Basics/comparisons.js
@@ -0,0 +1,7 @@
+var x = 3;
+var y = 5;
+
+var z = x == y; //x <= y; x != y; x >= y; x < y; x > y;
+document.writeln("num1: ", x);
+document.writeln("num2: ", y);
+document.writeln("num1 == num2: ", z);
